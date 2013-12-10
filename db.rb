520524f920ea3b900a7ed618cd9869b5007927cd
benzrf <benzrf@benzrf.com>
2013-12-10 13:33:49 -0500

    Added updating methods to db.

--- db.rb
+++ db.rb
@@ -1,3 +1,4 @@
+require './utils'
 require 'sinatra'
 require 'sinatra/sequel'
 
@@ -6,6 +7,11 @@ database.extension :pg_array
 
 def user(name)
   database[:users].where(name: name).first
+end
+
+def update_user(user)
+  where = user.slice(:name)
+  database[:users].where(where).update(user)
 end
 
 def comics(name)
@@ -18,5 +24,10 @@ end
 
 def user_comics(uname)
   database[:comics].where(uname: uname).all
+end
+
+def update_comic(comic)
+  where = comic.slice(:uname, :name)
+  database[:comics].where(where).update(comic)
 end
 
