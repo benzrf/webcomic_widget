3de02623be5348f41b2d7726d34a6df7e15b1dee
benzrf <benzrf@benzrf.com>
2013-12-10 13:23:21 -0500

    Added updated_between.

--- utils.rb
+++ utils.rb
@@ -1,8 +1,19 @@
+require 'date'
 require 'digest/sha1'
 
 HASH_SALT = ENV['HASH_SALT'] || ''
 def login_hash(user, password)
   salted = user.downcase + HASH_SALT + password
   Digest::SHA1.hexdigest(salted)
+end
+
+def updated_between?(schedule, from, til=Date.today)
+  return true unless from
+  from += 1
+  (from..til).each do |day|
+    updated_today = schedule[day.wday]
+    return true if updated_today
+  end
+  false
 end
 
